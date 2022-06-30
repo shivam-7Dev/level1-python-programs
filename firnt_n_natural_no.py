2202db2526ef46c64a6899bc7360c9a870bd04d9
Shivam Sharma <shivam7.dev@gmail.com>
2022-06-30 11:21:51 +0530

    test commit

--- firnt_n_natural_no.py
+++ firnt_n_natural_no.py
@@ -1,6 +1,6 @@
 from traceback import print_tb
 
-
-n=int(input("Enter first n natural number to print:"))
+# Test Comment
+n = int(input("Enter first n natural number to print:"))
 for i in range(1,n+1):
     print(i)
